39c3611a841146b463a9cff1b0a6055b0c2d0ae7
Ree2612 <khatwanireesa@gmail.com>
2025-07-09 00:25:25 +0530

    app.py

--- app.py
+++ app.py
@@ -105,3 +105,8 @@ def clear_log():
 # --- Run App ---
 if __name__ == "__main__":
     app.run(debug=True)
+    if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))  # ← important!
+    app.run(host='0.0.0.0', port=port)
+
